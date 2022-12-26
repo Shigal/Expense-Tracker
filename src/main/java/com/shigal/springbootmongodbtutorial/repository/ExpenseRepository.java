55a55c953491ba1a5482ef36d7db52ca84f812aa
Shigal <shiglaws@gmail.com>
2022-12-26 16:41:49 +0530

    added custom query

--- src/main/java/com/shigal/springbootmongodbtutorial/repository/ExpenseRepository.java
+++ src/main/java/com/shigal/springbootmongodbtutorial/repository/ExpenseRepository.java
@@ -5,7 +5,13 @@ package com.shigal.springbootmongodbtutorial.repository;/*
  */
 
 import com.shigal.springbootmongodbtutorial.model.Expense;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    @Query("{'name': ?0}")
+    Optional<Expense> findByName(String name);
 }
