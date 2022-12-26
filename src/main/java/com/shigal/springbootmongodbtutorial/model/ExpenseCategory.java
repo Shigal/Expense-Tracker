7491e500b8bcc83148674f62458e7e3d2173cdc2
Shigal <shiglaws@gmail.com>
2022-12-26 15:22:12 +0530

    added expense model class

---
+++ src/main/java/com/shigal/springbootmongodbtutorial/model/ExpenseCategory.java
@@ -0,0 +1,9 @@
+package com.shigal.springbootmongodbtutorial.model;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+public enum ExpenseCategory {
+    ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
